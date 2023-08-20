554f4360955a45055407969d016445b85b0b339b
FERHATSUMER <sumerf39@gmail.com>
2023-08-20 23:03:59 +0300

    on7fullpagenextjs

--- src/app/sinavlar/page.tsx
+++ src/app/sinavlar/page.tsx
@@ -1,8 +1,27 @@
 export default function Sinavlar() {
     return (
-      <div>
-        <h1>Sınavlar</h1>
-      </div>  
+        <div className="container">
+            <h1>Sınav Sayfası</h1>
+            <table className="exam-table">
+                <tbody>
+                    <tr>
+                        <td><strong>Sınav Adı:</strong></td>
+                        <td>Matematik Sınavı</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Sınav Tarihi:</strong></td>
+                        <td>15 Eylül 2023</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Dersin Öğretmen Adı:</strong></td>
+                        <td>Ahmet Yılmaz</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button className="button">Sınava Katıl</button>
+            <button className="button button-secondary">Ekle</button>
+        </div>
+    );
+};  
   
-    )
-  }
+  
