85357c894baf33a117cf37b8f0761a869420b451
FERHATSUMER <sumerf39@gmail.com>
2023-08-26 22:15:51 +0300

    on7fullpagenextjs

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -1,7 +1,12 @@
 export default function Content() {
   return (
     <div >
-        <h1>Bildirimler</h1>
+      <h1>On7 Akademi</h1>
+<div> 
+  <h2></h2>
+<button>Derse Katıl </button>
+</div>
+<button>Derse Katıl </button>
     </div>  
 
   )
