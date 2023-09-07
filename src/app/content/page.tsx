2ffd4da138b3ae1e74e3b3e332fd45e732610903
FERHATSUMER <sumerf39@gmail.com>
2023-09-07 20:38:53 +0300

    on7fullpagenextjs

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -3,15 +3,7 @@ import styles from './styles.module.css';
 export default function Content() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>On7 Akademi</h1>
-      <div className={styles.content}>
-        <h2 className={styles.subtitle}>DERSE KATILIM</h2>
-        <button className={styles.button}>Derse Katıl</button>
-      </div>
-      <div className={styles.content}>
-        <h3 className={styles.subtitle}>DERSE KATILIM</h3>
-        <button className={styles.button}>Derse Katıl</button>
-      </div>
+    
     </div>
   );
 }
