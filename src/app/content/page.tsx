c681cb909f9ac84425c89e0aaf8f9d9765bbd05b
FERHATSUMER <sumerf39@gmail.com>
2023-09-08 23:58:42 +0300

    on7fullpgenextjs

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -3,7 +3,10 @@ import styles from './styles.module.css';
 export default function Content() {
   return (
     <div className={styles.container}>
-    
+          <h1 className=''>On7 Akademi</h1>
+            <div className=''>
+              
+          </div>
     </div>
   );
 }
